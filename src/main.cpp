90856757ff83b4ac0a8aff7b7e35c404ab700e9e
ZOI_dayo <zoizoi.dayo@gmail.com>
2024-02-27 23:52:33 +0900

    FFmpegを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-  cout << "Hello World!" << endl;
+extern "C" {
+#include <libavutil/imgutils.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libavutil/pixdesc.h>
+#include <libswscale/swscale.h>
+}
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  const char *input_path = argv[1];
+  AVFormatContext *format_context = nullptr;
+  if (avformat_open_input(&format_context, input_path, nullptr, nullptr) != 0) {
+    std::cerr << "ファイルを開けませんでした" << std::endl;
+    return 1;
+  }
+  std::cout << format_context->iformat->name << std::endl;
 }
